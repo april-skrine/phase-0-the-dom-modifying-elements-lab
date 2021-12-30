421dfa2b4dea81ca01e6e15f06ccbb64df4dd484
April Skrine <april.skrine@gmail.com>
2021-12-30 15:27:30 -0600

    Done, DOM!

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main');
+document.querySelector('main').remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+// document.body.append(newHeader);
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
